bd0119148cc9246f30c4c7b558bf3015df5910aa
Garrett Biernat <garrett@gdbmedia.net>
2016-04-11 18:57:09 -0700

    finished sliders

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(function(){
-	var agevalue = 0;
+	var ageIvalue = 0;
+	var ageAvalue = 0;
 	var irvalue = 0;
-$('#age').slider({
+$('#ageI').slider({
 	formatter: function(value) {
-		agevalue = value;
-		$("#agevalue").text(agevalue);
+		ageIvalue = value;
+		$("#ageIvalue").text(ageIvalue);
+		$("#retire").html('<div class="form-group"><label for="points">Desired Age of Final Payment:</label><br><input id="ageA" data-slider-id="ageAslider" type="text" data-slider-tooltip="hide" class="inputs" data-slider-min="'+(ageIvalue+1)+'" data-slider-max="80" data-slider-step="1" data-slider-value="65"/><h2 id="ageAvalue" class="pull-right"></h2></div>');
+		$("#ageA").slider({
+	formatter: function(value) {
+		ageAvalue = value;
+		$("#ageAvalue").text(ageAvalue);
 	}
 });
+	}
+});
+
 $('#ir').slider({
 	formatter: function(value) {
 		irvalue = value;
