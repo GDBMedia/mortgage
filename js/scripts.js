56a7a3d70ed7ac7030858477a27935a228fc1435
Garrett Biernat <garrett@gdbmedia.net>
2016-04-12 14:28:21 -0700

    more slider styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(function(){
+	var balance = $("principal").val();
+	var monthly = $("payment").val();
+	var intrest = $("irvalue").val();
+	var monthly = $("payment").val();
 	var ageIvalue = 0;
 	var ageAvalue = 0;
 	var irvalue = 0;
-$('#ageI').slider({
+$('#age').slider({
 	formatter: function(value) {
 		ageIvalue = value;
 		$("#ageIvalue").text(ageIvalue);
+		//this how you output slider value
 		$("#retire").html('<div class="form-group"><label for="points">Desired Age of Final Payment:</label><br><input id="ageA" data-slider-id="ageAslider" type="text" data-slider-tooltip="hide" class="inputs" data-slider-min="' + (ageIvalue + 1) + '" data-slider-max="80" data-slider-step="1" data-slider-value="' + ageAvalue + '"/><h2 id="ageAvalue" class="pull-right"></h2></div>');
 		$("#ageA").slider({
 			formatter: function(value) {
@@ -18,8 +23,7 @@ $('#ageI').slider({
 
 $('#ir').slider({
 	formatter: function(value) {
-		irvalue = value;
-		$("#irvalue").text(irvalue+"%");
+		$("#irvalue").val(value+"%");
 	}
 });
 
