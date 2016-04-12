1c95db69b5e0e923b76bb597c5b2c3876fd9885a
Garrett Biernat <garrett@gdbmedia.net>
2016-04-11 18:25:22 -0700

    added html and styling
    added the use of sliders

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function(){
+	var agevalue = 0;
+	var irvalue = 0;
+$('#age').slider({
+	formatter: function(value) {
+		agevalue = value;
+		$("#agevalue").text(agevalue);
+	}
+});
+$('#ir').slider({
+	formatter: function(value) {
+		irvalue = value;
+		$("#irvalue").text(irvalue);
+	}
+});
+console.log(agevalue);
+console.log(irvalue);
+
+
+
+
+
+})
+
+
+
+
