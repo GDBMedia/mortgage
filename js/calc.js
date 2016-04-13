c2ecf2966a0fc6f52d733d0da93343b60f090f0a
Jacob Hefley <jacobhefley3@gmail.com>
2016-04-12 17:36:54 -0700

    a lot of work for nothing, logic is established

--- js/calc.js
+++ js/calc.js
@@ -1,92 +1,96 @@
-/// age, current payment, interest rate. balance -> age when done (age in years floored up)
+// /// age, current payment, interest rate. balance -> age when done (age in years floored up)
 
-// function mortgageCalculator (p, i, iA, cA, eA){
-function mortgageCalculator (p, i, iA, eA){
-	var principal = parseFloat(p); // set to usable number 
-	var interest = (parseFloat(i)/1200); // turn percentage into decimal and make the yearly interest into monthly
-	var inititalAge = parseInt(iA); // set to usable number 
-	var endAge = parseInt(eA); // set to usable number
-	var mortgageAge = (endAge - inititalAge)*12; // mortgage age = end age - initial age
-// end of mortgage (current - age = mortgagae length)
-	var currentAge = endAge/2; //// need to have passed (or calculated the current time in payments)
-	var calculatorChoice = "remaning"; // currently only supports standard 
-	var mortgage;
-	switch (calculatorChoice){
-		case "standard":
-			mortgage = calculateStandard(principal,interest,mortgageAge);
-			console.log("STANDARD calculatorChoice");
-	 		break;
-		case "remaning":
-			mortgage = calculateRemanining(principal,interest,intitalAge,currentAge,mortgageAge);
-			console.log("REMANING calculatorChoice");
-	 		break;
-	 	case "amortization":
-	 		mortgage = calculateAmortization();
-	 		console.log("AMORTIZATION calculatorChoice");
-	 		break;
-	 	case "ARM5":
-	 		mortgage = calculateARM5();
-	 		console.log("ARM5 calculatorChoice");
-	 		break;
-	 	case "ARM7":
-	 		mortgage = calculateARM7();
-	 		console.log("ARM5 calculatorChoice");
-	 		break;
-	 	default :
-	 		mortgage = errorCalulatorChoice();
-	 		break;
-	}
-	return mortgage;
-	// return mortgage;
-}
-
-
-
-
-
-
-
-
-
-function errorCalulatorChoice(){
-	// console.log("ERROR calculatorChoice"); // not needed
-	return "ERROR calculatorChoice";
-}
-function calculateStandard(principal, interest, mortgageAge){
-		// var P = 400000; //principle / initial amount borrowed
-		// var I = 3.5 / 100 / 12; //monthly interest rate
-		// var N = 30 * 12; //number of payments months
-		return calculateStandardMonthlyPayment(principal, rate, time);
-}
-function calculateRemaning (principal,interest,intitalAge,currentAge,mortgageAge){
-	balance = 
-
-	return 
-} 
-function calculateStandardMonthlyPayment(p, i, n){
-		// console.log(p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
-		return (p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
-}
-
-var print = mortgageCalculator(435678, 3.75, 43, 65);
-console.log(print);
-// console.log(mortgageCalculator());
-// 		var month
-// 		var monthlyPayment = 
-// 		var x = Math.pow((1 + (rate/12)), (time*12)),
-//             monthly = (amount*x*monthly_interest)/(x-1);
-
-// var M; //monthly mortgage payment
-// var P = 400000; //principle / initial amount borrowed
-// var I = 3.5 / 100 / 12; //monthly interest rate
-// var N = 30 * 12; //number of payments months
-
-// //monthly mortgage payment
-// M = monthlyPayment(P, N, I);
-
-// console.log(M);
-
-// function monthlyPayment(p, n, i) {
-//   return p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+// // function mortgageCalculator (p, i, iA, cA, eA){
+// function mortgageCalculator (p, i, iA, eA){
+// 	var principal = parseFloat(p); // set to usable number 
+// 	var interest = (parseFloat(i)/1200); // turn percentage into decimal and make the yearly interest into monthly
+// 	var inititalAge = parseInt(iA)*12; // set to usable number 
+// 	var endAge = parseInt(eA)*12; // set to usable number
+// 	var mortgageAge = (endAge - inititalAge); // mortgage age = end age - initial age
+// // end of mortgage (current - age = mortgagae length)
+// 	var currentAge = endAge/2; //// need to have passed (or calculated the current time in payments)
+// 	var calculatorChoice = "remaning"; // currently only supports standard 
+// 	var mortgage;
+// 	switch (calculatorChoice){
+// 		case "standard":
+// 			mortgage = calculateStandard(principal,interest,mortgageAge);
+// 			console.log("STANDARD calculatorChoice");
+// 	 		break;
+// 		case "remaning":
+// 			mortgage = calculateRemanining(principal,interest,intitalAge,currentAge,mortgageAge);
+// 			console.log("REMANING calculatorChoice");
+// 	 		break;
+// 	 	case "amortization":
+// 	 		mortgage = calculateAmortization();
+// 	 		console.log("AMORTIZATION calculatorChoice");
+// 	 		break;
+// 	 	case "ARM5":
+// 	 		mortgage = calculateARM5();
+// 	 		console.log("ARM5 calculatorChoice");
+// 	 		break;
+// 	 	case "ARM7":
+// 	 		mortgage = calculateARM7();
+// 	 		console.log("ARM5 calculatorChoice");
+// 	 		break;
+// 	 	default :
+// 	 		mortgage = errorCalulatorChoice();
+// 	 		break;
+// 	}
+// 	return mortgage;
+// 	// return mortgage;
 // }
+
+
+
+
+
+
+
+
+
+// function errorCalulatorChoice(){
+// 	// console.log("ERROR calculatorChoice"); // not needed
+// 	return "ERROR calculatorChoice";
+// }
+// function calculateStandard(principal, interest, mortgageAge){
+// 		// var P = 400000; //principle / initial amount borrowed
+// 		// var I = 3.5 / 100 / 12; //monthly interest rate
+// 		// var N = 30 * 12; //number of payments months
+// 		return calculateStandardMonthlyPayment(principal, rate, time);
+// }
+// function calculateRemaining (principal,interest,intitalAge,currentAge,mortgageAge){
+// 	var monthlyPayment = calculateStandardMonthlyPayment(principal, interest, mortgageAge);
+// 	var leftToPay = (monthlyPayment*mortgageAge) - (monthlyPayment*currentAge);
+
+// 	return 
+// } 
+// function calculateRemainingTimeOnMortgage(currentAge,mortgageAge){
+// 	return (mortgageAge-=currentAge);
+// }
+// function calculateStandardMonthlyPayment(p, i, n){
+// 		// console.log(p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+// 		return (p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+// }
+
+// var print = mortgageCalculator(435678, 3.75, 43, 65);
+// console.log(print);
+// // console.log(mortgageCalculator());
+// // 		var month
+// // 		var monthlyPayment = 
+// // 		var x = Math.pow((1 + (rate/12)), (time*12)),
+// //             monthly = (amount*x*monthly_interest)/(x-1);
+
+// // var M; //monthly mortgage payment
+// // var P = 400000; //principle / initial amount borrowed
+// // var I = 3.5 / 100 / 12; //monthly interest rate
+// // var N = 30 * 12; //number of payments months
+
+// // //monthly mortgage payment
+// // M = monthlyPayment(P, N, I);
+
+// // console.log(M);
+
+// // function monthlyPayment(p, n, i) {
+// //   return p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
+// // }
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,112 @@
-// // class form
-// function mortgageCalculator (input){
+class AmortizationTable{
+	constructor(principal, interest, time){
+		this.principal = parseFloat(principal);
+		this.balance = this.principal;
+		this.interest = parseFloat(interest)/1200; /// inputed as APR, converted to monthly rate
+		this.time = parseFloat(time)*12; //inputed as years so months 
+		this.monthlyPayment = (this.principal * this.interest * (Math.pow(1 + this.interest, this.time)) / (Math.pow(1 + this.interest, this.time) - 1));
+		this.row = new Float64Array[7*this.time];//// 1-dimmenisonal 7 values, then new row
+		this.table = [this.time];
+	}
+	getMonthlyPayment(){
+		console.log(this.monthlyPayment);
+		return this.monthlyPayment;
+	}
+	//monthly payment , interestpaid, priniciapl paid, remaining balance-=principal paid, total interest, total principal, total paid 
+	tableGenerator(){
+		////FIRST TABLE ROW
+		this.row[0]=this.monthlyPayment;
+		this.row[1]=(this.balance*this.row[0]);//interest=balance*monthly interest
+		this.row[2]=(this.row[0]-this.row[1]);//remaining balance-=principal paid
+		this.row[3]=(this.row[3]-this.row[2]);//balance-=principalPaid
+		this.row[4]=this.row[3];//†otalinterestpaid+=interest
+		this.row[5]=this.row[2];//totalprincipalpaid+=principal
+		this.row[6]=(this.row[3]+this.row[2]);//totalpaid=†otalinterestpaid+totalprincipalpaid
+		// while(this.row[3]>0)
+		for(var i=1; i<this.time; i+=7){
+				this.row[0+i]=this.row[0];//NEED TO EDIT this.row[0]
+				this.row[1+i]=(this.row[i-5]*this.row[0]);//interest=balance*monthly interest
+				this.row[2+i]=(this.row[0]-this.row[1+i]);//NEED TO EDIT this.row[0]
+				this.row[3+i]=(this.row[i-7]-this.row[i-5]);//balance-=principalPaid
+				this.row[4+i]=this.row[3];//†otalinterestpaid+=interest
+				this.row[5+i]=this.row[2];//totalprincipalpaid+=principal
+				this.row[6+i]=this.row[3]+this.row[2];//totalpaid=†otalinterestpaid+totalprincipalpaid
+				// if(this.row[3]<)
+			}
+		}
+	}
+	[0 = monthlyPayment], [1 = remainingBlance*monthlyinterest], [2 = monthlyPayment- [1]]
+}
+// //  calculateMonthlyPayment(p, i, n){
+// // 		// console.log(p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+// // 		return (p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+// // }	
 
 // }
-// function processObject{
 
+// // // class form
+// // function mortgageCalculator (input){
+
+// // }
+// // function processObject{
+
+// // }
+// // givern monehtly payment let user know when done 
+// function calculateStandardMonthlyPayment(p, i, n){
+// 		// console.log(p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+// 		return (p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
 // }
-// givern monehtly payment let user know when done 
-function calculateStandardMonthlyPayment(p, i, n){
-		// console.log(p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
-		return (p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
-}
 
-function endOfMortgageBasedOnMonthlyPayment(input){
-	var principal = parseFloat(input[0]);
-	var monthlyPayment = parseFloat(input[1]);
-	var startAge = parseFloat(input[3]);
-	// var endAge = parsefloat(input[4]);
-	input[4] = (principal/monthlyPayment)/12;
-	return input[4];
-}
+// function endOfMortgageBasedOnMonthlyPayment(input){
+// 	var principal = parseFloat(input[0]);
+// 	var monthlyPayment = parseFloat(input[1]);
+// 	var startAge = parseFloat(input[3]);
+// 	// var endAge = parsefloat(input[4]);
+// 	input[4] = (principal/monthlyPayment)/12;
+// 	return input[4];
+// }
+// function mortrgageAmortizationTable(){
+// 	var rowOfArray = new Float64Array();
+// 	var returnArray = [];
+	
+// 	var 
 
 
 
+
+
+
+// 	// console.log("ERROR calculatorChoice"); // not needed
+// 	return "ERROR calculatorChoice";
+// }
+// P = principal, the initial amount of the loan
+// I = the annual interest rate (from 1 to 100 percent)
+// L = length, the length (in years) of the loan, or at least the length over which the loan is amortized.
+
+// // The following assumes a typical conventional loan where the interest is compounded monthly. 
+// // First I will define two more variables to make the calculations easier:
+
+// J = monthly interest in decimal form = I / (12 x 100)
+// N = number of months over which loan is amortized = L x 12
+
+// this.principal * (J/(1-(1+)))
+// M = P * (J/(1-(1+J) ** -N))
 
 $(function(){
 	var input= [];
 	$("#submit").click(function(event){
-		input[0] = $("#principal").val();
-		input[1] = $("#payment").val();
-		input[2] = $("#interest").val();
-		input[3] = $("#bday").val();
-		input[4] = $("#done").val();
-		input[5]= $("#none").val();
-		// endOfMortgageBasedOnMonthlyPayment(input);
-		endOfMortgageBasedOnMonthlyPayment(input)
-		$("#done").val(endOfMortgageBasedOnMonthlyPayment(input));
-		console.log(input);
+		test = new AmortizationTable("500000.00","3.5","30");
+		test.getMonthlyPayment();
+		// input[0] = $("#principal").val();
+		// input[1] = $("#payment").val();
+		// input[2] = $("#interest").val();
+		// input[3] = $("#bday").val();
+		// input[4] = $("#done").val();
+		// // input[5]= $("#none").val();
+		// // endOfMortgageBasedOnMonthlyPayment(input);
+		// endOfMortgageBasedOnMonthlyPayment(input)
+		// $("#done").val(endOfMortgageBasedOnMonthlyPayment(input));
+		// console.log(input);
 		event.preventDefault();
 	});
 // 	var agevalue = $("#size").val();
@@ -54,8 +125,9 @@ $(function(){
 // });
 // console.log(agevalue);
 // console.log(irvalue);
+event.preventDefault();
 
-
+	
 
 
 
