84e23b40c0c0c3c45cc1dba867dac87925ea6b5b
garrett <garrett@gdbmedia.net>
2016-04-14 13:40:00 -0700

    updated html

--- js/scripts.js
+++ js/scripts.js
@@ -76,6 +76,15 @@ $(function(){
 					 interval: false
 			 });
 	 });
+	
+    $(document).bind('keyup', function(e) {
+        if(e.which == 39){
+            $('.carousel').carousel('next');
+        }
+        else if(e.which == 37){
+            $('.carousel').carousel('prev');
+        }
+    });
 
 
 });
