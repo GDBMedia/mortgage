94a030f9240493a60fee79ca198e57a7f85ef100
garrett <garrett@gdbmedia.net>
2016-04-14 12:42:27 -0700

    Merge branch 'master' into newcarousel
    # Conflicts:
    #	carousel.html

--- js/scriptsC.js
+++ js/scriptsC.js
@@ -1,27 +1,20 @@
 $(function(){
-	var ageIvalue = 0;
-	var ageAvalue = 0;
+	var agevalue = 0;
 	var irvalue = 0;
-$('#ageI').slider({
+$('#age').slider({
 	formatter: function(value) {
-		ageIvalue = value;
-		$("#ageIvalue").text(ageIvalue);
-		$("#retire").html('<div class="form-group"><label for="points">Desired Age of Final Payment:</label><br><input id="ageA" data-slider-id="ageAslider" type="text" data-slider-tooltip="hide" class="inputs" data-slider-min="' + (ageIvalue + 1) + '" data-slider-max="80" data-slider-step="1" data-slider-value="' + ageAvalue + '"/><h2 id="ageAvalue" class="pull-right"></h2></div>');
-		$("#ageA").slider({
-			formatter: function(value) {
-			ageAvalue = value;
-			$("#ageAvalue").text(ageAvalue);
+		agevalue = value;
+		$("#agevalue").text(agevalue);
 	}
 });
-	}
-});
-
 $('#ir').slider({
 	formatter: function(value) {
 		irvalue = value;
-		$("#irvalue").text(irvalue+"%");
+		$("#irvalue").text(irvalue);
 	}
 });
+console.log(agevalue);
+console.log(irvalue);
 
 
 
@@ -33,3 +26,4 @@ $('.carousel').each(function(){
 	 });
 
 })
+
